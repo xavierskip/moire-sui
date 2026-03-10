8b7362fdd21c11dc112d2b157c2e38679028e66a
xavier skip <xavierskip@gmail.com>
2026-03-10 19:13:07 +0800

    Update config values for title, author, and theme
    like a selfhost weibo

--- moire.config.ts
+++ moire.config.ts
@@ -1,10 +1,10 @@
 export const config = {
-  title: "Moire",
-  author: "Aeris",
-  theme: "classic",
+  title: "碎碎念",
+  author: "xavierskip",
+  theme: "pixel",
   pageSize: 20,
   order_by: "created",
-  description: "Sync your thoughts from Apple Notes by Shortcuts.",
+  description: "like a selfhost weibo",
   keywords: "memo, thought, sync, apple notes, shortcuts, ios",
   url: "https://moire.blog"
 };
